fd19d341fe4e94c1a995af1a27337b139faa6363
Andrew <a--i@list.ru>
2014-08-08 10:57:30 +0400

    работает через https

--- src/main/scala/ai/spray/server/Main.scala
+++ src/main/scala/ai/spray/server/Main.scala
@@ -1,15 +1,15 @@
 package ai.spray.server
 
 import ai.spray.service.OAuth2Service
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
 
 /**
  * Created by Andrew on 06.08.2014.
  */
-object Main {
-  def main(args: Array[String]) {
+object Main extends OAuth2SslConfiguration{
+  def main(args: Array[String])  {
     implicit val system = ActorSystem("sprayOAuth2System")
     IO(Http) ! Http.Bind(system.actorOf(Props[OAuth2Service]), interface = "localhost", port = 8080)
   }
