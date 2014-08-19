8fa9c253735e32b3629189e48f6b26b9cdb3020f
Andrew <a--i@list.ru>
2014-08-20 00:10:53 +0400

    переход на akka http

--- src/main/scala/ai/spray/server/Main.scala
+++ src/main/scala/ai/spray/server/Main.scala
@@ -1,18 +1,42 @@
 package ai.spray.server
 
-import ai.spray.service.OAuth2ServiceActor
-import akka.actor.{ActorSystem, Props}
+import akka.actor.ActorSystem
+import akka.http.Http
+import akka.http.model.{StatusCodes, HttpResponse}
 import akka.io.IO
-import spray.can.Http
+import akka.pattern._
+import akka.stream.{MaterializerSettings, FlowMaterializer}
+import akka.stream.scaladsl.Flow
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.concurrent.Future
+
 
 /**
  * Created by Andrew on 06.08.2014.
  * Запуск провайдера
  */
-object Main extends OAuth2SslConfiguration {
+object Main {
+
 
   def main(args: Array[String]) {
     implicit val system = ActorSystem("sprayOAuth2System")
-    IO(Http) ! Http.Bind(system.actorOf(Props[OAuth2ServiceActor]), interface = "localhost", port = 8080)
+    implicit val ec = system.dispatcher
+    implicit val timeout = Timeout(5.seconds)
+    val settings = MaterializerSettings()
+    val materializer = FlowMaterializer(settings)
+
+    val future: Future[Any] = IO(Http) ? Http.Bind(interface = "localhost", port = 8080)
+    future.onSuccess({
+      case serverBinding: Http.ServerBinding =>
+        println("Server started, listening on: " + serverBinding.localAddress)
+        Flow(serverBinding.connectionStream).foreach(conn => {
+          Flow(conn.requestPublisher)
+            .map(request => HttpResponse(status = StatusCodes.OK, entity = request.toString))
+            .produceTo(FlowMaterializer(MaterializerSettings()), conn.responseSubscriber)
+          println("Client connected from: " + conn.remoteAddress)
+        }
+        ).consume(materializer)
+    })
   }
 }
