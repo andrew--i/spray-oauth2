4e14297f60f7492a3992b8f2d548f1fbbcba0038
Andrew <a--i@list.ru>
2014-08-20 23:50:00 +0400

    восстановлены тесты

--- src/test/scala/ai/akka/actor/RequestProcessingActorTest.scala
+++ src/test/scala/ai/akka/actor/RequestProcessingActorTest.scala
@@ -12,18 +12,23 @@ import scala.concurrent.duration._
  * Created by Андрей Смирнов on 20.08.2014.
  */
 class RequestProcessingActorTest extends Specification with NoTimeConversions {
+
+  def get(clientId: String): HttpRequest = {
+    HttpRequest(HttpMethods.GET, uri = "/oauth/authorize")
+  }
+
   "The RequestProcessingActor" should {
     "handle /oauth/authorize GET requests" in new AkkaTestkitSpecs2Support {
       within(500.millis) {
         system.actorOf(Props[RequestProcessingActor]) ! HttpRequest(HttpMethods.GET, uri = "/oauth/authorize")
-        expectMsgType[HttpResponse] must not beNull
+        (expectMsgType[HttpResponse] must not).beNull
       }
     }
 
-    "handle /oauth/authorize GET requests with clientId and responseType parameters" in new AkkaTestkitSpecs2Support {
+    "handle /oauth/authorize GET requests with clientId" in new AkkaTestkitSpecs2Support {
       within(500.millis) {
-        system.actorOf(Props[RequestProcessingActor]) ! HttpRequest(HttpMethods.GET, uri = "/oauth/authorize?" + Constants.CLIENT_ID + "=123&" + Constants.RESPONSE_TYPE + "=resp_type")
-        expectMsg[HttpResponse](HttpResponse(StatusCodes.OK))
+        system.actorOf(Props[RequestProcessingActor]) ! HttpRequest(HttpMethods.GET, uri = "/oauth/authorize?" + Constants.CLIENT_ID + "=1")
+        expectMsgType[HttpResponse].status mustEqual StatusCodes.OK
       }
     }
 
